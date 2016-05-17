953e249c407d0a9695e861dd85efc143766fd14b
Andrea Bernardo Ciddio <bcandrea@gmail.com>
2016-05-17 16:53:50 +0100

    Move build path to the package directory

--- debian/build/build.sh
+++ debian/build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mk-build-deps -i -r -t 'apt-get -y' /opt/consul-deb/debian/control
 pushd /opt/consul-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
-cp /output/* /tmp/consul-deb
+cp /tmp/build-area/* /tmp/consul-deb
